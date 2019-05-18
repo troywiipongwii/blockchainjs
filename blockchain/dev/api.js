115793cbeb8061411604f1ffd6aefe2a80d88664
troywiipongwii <troy.wiipongwii33@gmail.com>
2019-05-18 19:29:49 -0400

    added api to interact with blockchain

---
+++ blockchain/dev/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+ app.use(bodyParser.json());
+ app.use(bodyParser.urlencoded({extended: false}))
+
+app.get('/blockchain', function (req, res) {
+  
+});
+
+ app.post('/transaction', function (req, res) {
+ 	res.send('It works  haha good!!!')
+  
+});
+
+ app.get('/mine', function (req, res) {
+  
+});
+
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000...');
+});
