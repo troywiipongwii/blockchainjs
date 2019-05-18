f492da2345d420bfc16a206e2068f7c573624d09
troywiipongwii <troy.wiipongwii33@gmail.com>
2019-05-18 13:58:37 -0400

    update on blockchain construction and methods along with tests

---
+++ blockchain/dev/blockchain.js
@@ -0,0 +1,43 @@
+
+function Blockchain() {
+	this.chain = [];
+	this.newTransactions = [];
+
+}
+
+
+Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
+	const newBlock = {
+		index: this.chain.length + 1,
+		timestamp: Date.now(),
+		transactions: this.newTransactions,
+		nonce: nonce,
+		hash: hash,
+		previousBlockHash: previousBlockHash
+	};
+
+	this.newTransactions = [];
+	this.chain.push(newBlock);
+
+	return newBlock;
+};
+
+Blockchain.prototype.getLastBlock = function() {
+	return this.chain[this.chain.length -1];
+}
+
+Blockchain.prototype.createNewTransaction = function(amount,sender, recipient) {
+	const newTransaction = {
+		amount: amount,
+		sender: sender,
+		recipient: recipient
+	};
+
+	this.newTransactions.push(newTransaction);
+
+	return this.getLastBlock()['index'] + 1
+}
+
+
+
+module.exports = Blockchain;
---
+++ blockchain/dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(3333, 'joddignasodng93','393jr;ajr9ejj;j');
+
+bitcoin.createNewTransaction(500,'Johnffff','Jacob32fafdf')
+
+console.log(bitcoin);
